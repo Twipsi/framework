844d392d3cab3cbb44114d6005faefcfacce1431
Petrik Gábor <admin@twipsi.com>
2022-10-01 21:23:07 +0200

    Update CommandLoader.php

--- src/Twipsi/Foundation/Console/CommandLoader.php
+++ src/Twipsi/Foundation/Console/CommandLoader.php
@@ -10,13 +10,52 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Twipsi\Foundation;
+namespace Twipsi\Foundation\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
+use Twipsi\Foundation\Application\Application;
 
 class CommandLoader implements CommandLoaderInterface
 {
-  public function __construct(Application $app, array $commandMap)
-  {
-    $this->app = $app;
-    $this->commandMap = $commandMap;
-  }
+    /**
+     * Application instance.
+     *
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * Commands that need DI.
+     *
+     * @var array
+     */
+    protected array $commandMap = [];
+
+    /**
+     * Construct Command loader.
+     *
+     * @param Application $app
+     * @param array $commandMap
+     */
+    public function __construct(Application $app, array $commandMap)
+    {
+        $this->app = $app;
+        $this->commandMap = $commandMap;
+    }
+
+    public function get(string $name): Command
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has(string $name): bool
+    {
+        // TODO: Implement has() method.
+    }
+
+    public function getNames(): array
+    {
+        // TODO: Implement getNames() method.
+    }
 }
